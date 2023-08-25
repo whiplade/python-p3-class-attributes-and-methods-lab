2cbd31ce44164c72738a7330a77b17695ad70601
whiplade <nmawira13@gmail.com>
2023-08-25 10:48:51 +0300

    completed assignment

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,9 @@
 class Song:
-    pass
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        self.count = 0
+
+    def count(self):
+        pass
